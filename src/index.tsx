5c4f4f7d72ce3b6f88d1a49a4966fa96be0eb408
Chris <chris@zoid.com.au>
2023-10-15 14:23:27 +1100

    Remove Application Insights

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import '@webscopeio/react-textarea-autocomplete/style.css';
 import {createRoot} from 'react-dom/client';
 import Root from './containers/Root';
-import {ApplicationInsights} from '@microsoft/applicationinsights-web';
 import './app.global.css';
 import {
   getThemeModeFromStore,
@@ -22,14 +21,6 @@ Sentry.init({
   environment: MODE,
 });
 
-const appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey: 'b3c046b8-137c-47f3-b28d-9049abfa9fe8',
-    /* ...Other Configuration Options... */
-  },
-});
-appInsights.loadAppInsights();
-appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
 const elem = document.getElementById('root');
 if (elem) {
   const root = createRoot(elem);
