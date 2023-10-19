7f77064285270f28e4cd25b35f447238cb222fd2
Chris <chris@zoid.com.au>
2023-10-20 10:40:10 +1100

    Remove heart from 3D view

--- src/components/three-fiber/case.tsx
+++ src/components/three-fiber/case.tsx
@@ -1,7 +1,6 @@
 import {KeyColorType} from '@the-via/reader';
 import React from 'react';
 import {useMemo} from 'react';
-import {shallowEqual} from 'react-redux';
 import {getSelectedDefinition} from 'src/store/definitionsSlice';
 import {useAppSelector} from 'src/store/hooks';
 import {getSelectedTheme} from 'src/store/settingsSlice';
@@ -198,102 +197,6 @@ const SimplePlate: React.FC<{width: number; height: number}> = ({
   );
 };
 
-const Heart = React.memo(
-  (props: {caseWidth: number; caseHeight: number; color: string}) => {
-    const heartAngle = Math.atan(2 / props.caseWidth);
-
-    const midXOffset = (80 + -30) / 2;
-    const radius = 2;
-    const bezelSize = 1;
-    const caseYHeight = -(-props.caseWidth - bezelSize * 2 - radius * 2);
-    const backHeight = caseYHeight / Math.cos(heartAngle);
-    const scale = (0.1 * backHeight) / 22;
-    const midYOffset = 95;
-    const heartHeight = 95 * scale;
-    const midMidOffset = (backHeight - heartHeight) / 2;
-    const heartShape = useMemo(() => {
-      const shape = new Shape();
-      shape.moveTo(scale * (25 - midXOffset), scale * (25 - midYOffset));
-      shape.bezierCurveTo(
-        scale * (25 - midXOffset),
-        scale * (25 - midYOffset),
-        scale * (20 - midXOffset),
-        scale * (0 - midYOffset),
-        scale * (0 - midXOffset),
-        scale * (0 - midYOffset),
-      );
-      shape.bezierCurveTo(
-        scale * (-30 - midXOffset),
-        scale * (0 - midYOffset),
-        scale * (-30 - midXOffset),
-        scale * (35 - midYOffset),
-        scale * (-30 - midXOffset),
-        scale * (35 - midYOffset),
-      );
-      shape.bezierCurveTo(
-        scale * (-30 - midXOffset),
-        scale * (55 - midYOffset),
-        scale * (-10 - midXOffset),
-        scale * (77 - midYOffset),
-        scale * (25 - midXOffset),
-        scale * (95 - midYOffset),
-      );
-      shape.bezierCurveTo(
-        scale * (60 - midXOffset),
-        scale * (77 - midYOffset),
-        scale * (80 - midXOffset),
-        scale * (55 - midYOffset),
-        scale * (80 - midXOffset),
-        scale * (35 - midYOffset),
-      );
-      shape.bezierCurveTo(
-        scale * (80 - midXOffset),
-        scale * (35 - midYOffset),
-        scale * (80 - midXOffset),
-        scale * (0 - midYOffset),
-        scale * (50 - midXOffset),
-        scale * (0 - midYOffset),
-      );
-      shape.bezierCurveTo(
-        scale * (35 - midXOffset),
-        scale * (0 - midYOffset),
-        scale * (25 - midXOffset),
-        scale * (25 - midYOffset),
-        scale * (25 - midXOffset),
-        scale * (25 - midYOffset),
-      );
-      return shape;
-    }, [props.caseWidth, props.caseHeight, props.color]);
-
-    const extrudeSettings = {
-      depth: 4,
-      bevelEnabled: true,
-      bevelSegments: 10,
-      bevelSize: 1,
-      bevelThickness: 1,
-    };
-
-    return (
-      <mesh
-        position={[
-          -backHeight + midMidOffset,
-          radius * 2 + bezelSize * 2 + props.caseHeight / 2,
-          0,
-        ]}
-        scale={1}
-        rotation={[Math.PI / 2, heartAngle, Math.PI / 2]}
-      >
-        <extrudeGeometry
-          attach="geometry"
-          args={[heartShape, extrudeSettings]}
-        />
-        <meshPhongMaterial color={props.color} transparent={true} opacity={1} />
-      </mesh>
-    );
-  },
-  shallowEqual,
-);
-
 const makeShape2 = (layoutHeight: number) => {
   const offsetXMultiplier = Math.tan((Math.PI * 7.5) / 180);
   const bottomWidth = 10;
@@ -346,7 +249,6 @@ export const Case = React.memo((props: {width: number; height: number}) => {
   const theme = useAppSelector(getSelectedTheme);
   const outsideColor = useMemo(() => theme[KeyColorType.Accent].c, [theme]);
   const innerColor = '#212020';
-  const heartColor = useMemo(() => theme[KeyColorType.Accent].t, [theme]);
   const properWidth =
     props.width * KeycapMetric.keyXPos - KeycapMetric.keyXSpacing;
   const properHeight =
@@ -361,18 +263,8 @@ export const Case = React.memo((props: {width: number; height: number}) => {
   }, [properHeight]);
   const bezelSize = 1;
   const bevelSegments = 10;
-  // TODO: Heart positioning
-  const offsetXMultiplier = Math.tan((Math.PI * 7.5) / 180);
-  const bottomWidth = 10;
-  const heartCaseWidth =
-    bottomWidth + offsetXMultiplier * (properHeight + insideBorder * 2);
   return (
     <group scale={1} position-z={-bezelSize * 2} rotation-y={-Math.PI / 2}>
-      <Heart
-        caseWidth={heartCaseWidth}
-        caseHeight={properHeight}
-        color={heartColor}
-      />
       <mesh
         position={[-bezelSize, 0, -outsideCaseThickness / 2]}
         castShadow={true}
